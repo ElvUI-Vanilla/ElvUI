38725334d9bdc1402894c45f88c9598dacb203d5
Crum <stschleyer89@gmail.com>
2017-12-15 21:53:39 -0600

    update Greeting skin

--- 2/3/4/5/6/7/ElvUI/Modules/Skins/Blizzard/Greeting.lua
+++ 2/3/4/5/6/7/ElvUI/Modules/Skins/Blizzard/Greeting.lua
@@ -3,8 +3,10 @@ local S = E:GetModule("Skins");
 
 --Cache global variables
 --Lua functions
-local _G = getfenv()
+local _G = _G
+local find, gsub = string.find, string.gsub
 --WoW API / Variables
+local HookScript = HookScript
 
 local function LoadSkin()
 	-- if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.greeting ~= true then return end
@@ -17,11 +19,12 @@ local function LoadSkin()
 		E:Kill(QuestGreetingFrameHorizontalBreak)
 		AvailableQuestsText:SetTextColor(1, 1, 0)
 		S:HandleScrollBar(QuestGreetingScrollFrameScrollBar)
-		for i=1, MAX_NUM_QUESTS do
+		for i = 1, MAX_NUM_QUESTS do
 			local button = _G["QuestTitleButton"..i]
+			button:SetTextColor(1, 1, 1)
 			if button:GetFontString() then
-				if button:GetFontString():GetText() and string.gfind(button:GetFontString():GetText(), "|cff000000") then
-					button:GetFontString():SetText(string.gsub(button:GetFontString():GetText(), "|cff000000", "|cffFFFF00"))
+				if button:GetFontString():GetText() and find(button:GetFontString():GetText(), "|cff000000") then
+					button:GetFontString():SetText(gsub(button:GetFontString():GetText(), "|cff000000", "|cffFFFF00"))
 				end
 			end
 		end
