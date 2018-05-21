fadd92b36c6cd69ea69e7dbc881691c72bf19725
Logan Payton <Logan.Payton@javs.com>
2018-05-21 03:16:45 -0400

    Count nil

--- ElvUI/Libraries/oUF/elements/auras.lua
+++ ElvUI/Libraries/oUF/elements/auras.lua
@@ -227,7 +227,7 @@ local function updateIcon(element, unit, index, offset, filter, isDebuff, visibl
 	end
 
 	if button.icon then button.icon:SetTexture(texture) end
-	if button.count then button.count:SetText(count > 1 and count) end
+	-- if button.count then button.count:SetText(count > 1 and count) end
 
 	local size = element.size or 16
 	button:SetWidth(size)
